2971fe1bb4e94946ff24b640b1596c29303d7abd
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-11-17 19:45:00 +0200

    feat: add task solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -183,8 +183,8 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
@@ -565,8 +565,21 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const dictionary = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  return arr.sort((a, b) => dictionary[a] - dictionary[b]);
 }
 
 /**
@@ -588,8 +601,21 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const l = arr.length;
+
+  if (l % 2 !== 0) {
+    const centerNumber = (l - 1) / 2;
+    const head = arr.slice(0, centerNumber);
+    const tail = arr.slice(centerNumber + 1);
+
+    return tail.concat(arr[centerNumber]).concat(head);
+  }
+
+  const head = arr.slice(0, l / 2);
+  const tail = arr.slice(l / 2);
+
+  return tail.concat(head);
 }
 
 module.exports = {
